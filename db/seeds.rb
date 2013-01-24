2339803cac1aadd413ab2a718e36ff173d8d2f00
Ben Syzek <syzek.1@gmail.com>
2013-01-24 10:21:47 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+states = State.create([ { name: 'North Carolina' },
+                        { name: 'Ohio' }])
+
+cities = City.create([  { name: 'Raleigh', state_id: states.first.id },
+                        { name: 'Durham', state_id: states.first.id },
+                        { name: 'Cincinnati', state_id: states.last.id }])
+
+neighborhoods = Neighborhood.create([ { name: 'Five Points', city_id: cities.first.id },
+                                      { name: 'Glenwood South', city_id: cities.first.id },
+                                      { name: 'Cameron Village', city_id: cities.first.id },
+                                      { name: 'Anderson', city_id: cities.last.id },
+                                      { name: 'Hyde Park', city_id: cities.last.id }])
