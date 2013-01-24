fa4000896be75956c49ddfee7a78bb94fe298033
Ben Syzek <syzek.1@gmail.com>
2013-01-24 09:34:35 -0500

    Update Gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Neighborhoods::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assests.initialze_on_precompile = false  
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
