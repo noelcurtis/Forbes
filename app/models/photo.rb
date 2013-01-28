38e26809fc456b953d538e269bd334b5504ba1db
Ben Syzek <syzek.1@gmail.com>
2013-01-28 17:02:43 -0500

    Fix paperclip bug

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessible :image, :user_id, :neighborhood_id
   
-  has_attached_file :image, :styles => { :medium => "200x200>" }
+  has_attached_file :image, :styles => { :medium => "300x300>" }
  
   belongs_to :user
   belongs_to :neighborhood 
