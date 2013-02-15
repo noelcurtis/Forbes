8679b3d5009b0d55e6293cf30ba7c5dd4476d6ff
Ben Syzek <syzek.1@gmail.com>
2013-02-15 09:24:49 -0500

    Added Indian Citites and States to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
 
+file = CSV.read("db/seeds/indian_states_and_cities.csv")
 
-states = State.create([ { name: 'North Carolina' },
-                        { name: 'Ohio' }])
-
-cities = City.create([  { name: 'Raleigh', state_id: states.first.id },
-                        { name: 'Durham', state_id: states.first.id },
-                        { name: 'Cincinnati', state_id: states.last.id }])
-
-#neighborhoods = Neighborhood.create([ { name: 'Five Points', city_id: cities.first.id },
-#                                      { name: 'Glenwood South', city_id: cities.first.id },
-#                                      { name: 'Cameron Village', city_id: cities.first.id },
-#                                      { name: 'Anderson', city_id: cities.last.id },
-#                                      { name: 'Hyde Park', city_id: cities.last.id }])
+file.each do |pair|
+  State.create!(name: pair.last) unless State.exists?(name: pair.last)
+  City.create!(name: pair.first, state_id: State.find_by_name(pair.last).id)
+end
