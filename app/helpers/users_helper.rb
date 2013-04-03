93160a36f8ed288b077578ff88280408ddef4ff3
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2013-04-03 00:01:24 -0400

    fix ticket 772 766 770

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  # I know this is ugly but its not my fault :(
+  def current_user_or(user, user_id)
+    if user_id.present?
+      user.id == user_id ? user : User.find(user_id)
+    else
+      user
+    end
+  end
 end
