74e0124a485dec063bff12db6ebc6b6f353d44e4
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2013-04-03 23:27:24 -0400

    fix issue with 770

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
-  # I know this is ugly but its not my fault :(
-  def current_user_or(user, user_id)
+  def user_nav_link(user, user_id)
+    current_user = find_user(user, user_id)
+    link_to current_user.full_name, current_user, class: 'profile-name'
+  end
+
+  def find_user(user, user_id)
     if user_id.present?
       user.id == user_id ? user : User.find(user_id)
     else
