8d7e27acba021cf9c306f7e22c55014ce1cda10e
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2013-04-18 23:15:15 -0400

    hide edit button when places page

--- app/helpers/neighborhoods_helper.rb
+++ app/helpers/neighborhoods_helper.rb
@@ -2,4 +2,8 @@ module NeighborhoodsHelper
   def is_primary?(neighborhood)
     current_user.primary_neighborhood == neighborhood
   end
+
+  def show_edit?
+    params[:controller] == 'neighborhoods'
+  end
 end
